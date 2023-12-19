8aed7bf4ecf36b7d810c014c2a99267e81ee7c70
gadriano11 <adrgy001@mymail.unisa.edu.au>
2023-12-19 20:13:09 +1100

    cleaned folder structure, happy with layout, await server-side impl

--- static/js/map.js
+++ static/js/map.js
@@ -7,7 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
         code3: country.country_code, // assuming 'country_code' is the ISO 3166-1 alpha-3 country code
         name: country.country_name,
         value: country.corruption_index_score,
-        rank: country.rank
+        rank: country.rank,
+        incomeLessThan215: country.income_less_than_two_one_five,
+        maleGrossEnrolment: country.male_gross_enrolment,
+        femaleGrossEnrolment: country.female_gross_enrolment
       }));
 
       // Create the Highcharts map
@@ -54,19 +57,22 @@ document.addEventListener('DOMContentLoaded', function() {
             events: {
               click: function() {
                 // Open modal with details on click
-                const modalTitle = document.querySelector('.modal-title');
-                const modalText = document.querySelector('#modal-text');
+                const modalTitleMap = document.getElementById('modalTitleMap');
+                const modalTextMap = document.getElementById('modalTextMap');
 
                 // Set the title and text of the modal
-                modalTitle.innerHTML = this.name;
-                modalText.innerHTML = `
-                                    <strong>Corruption Index Score:</strong> ${this.value}<br/>
-                                    <strong>Rank:</strong> ${this.rank}
-                                `;
+                modalTitleMap.textContent = this.name;
+                modalTextMap.innerHTML = `
+                  <strong>Corruption Index Score:</strong> ${this.value}<br/>
+                  <strong>Rank:</strong> ${this.rank}<br/>
+                  <strong>Income less than $2.15:</strong> ${this.incomeLessThan215}<br/>
+                  <strong>Male Gross Enrolment:</strong> ${this.maleGrossEnrolment}<br/>
+                  <strong>Female Gross Enrolment:</strong> ${this.femaleGrossEnrolment}<br/>
+                `;
 
                 // Show the modal
-                const myModal = new bootstrap.Modal(document.getElementById('myModal'));
-                myModal.show();
+                const myModalMap = new bootstrap.Modal(document.getElementById('myModalMap'));
+                myModalMap.show();
               }
             }
           },
